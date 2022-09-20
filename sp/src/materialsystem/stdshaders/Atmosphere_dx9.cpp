606c24f2a005245a0ee86f2d4eecfdf3cf3f7ebd
Dkxtro2 <95775192+WhiteRedDragons@users.noreply.github.com>
2022-09-20 13:48:07 +0200

    Updated some Comments on the Shader

--- sp/src/materialsystem/stdshaders/Atmosphere_dx9.cpp
+++ sp/src/materialsystem/stdshaders/Atmosphere_dx9.cpp
@@ -1,6 +1,8 @@
 //==================================================================================================
 //
-// Fake Atmosphere shader with lightsource to alpha calculations
+// Fake Atmosphere shader with consideration for lightsources. Written by White_Red_Dragons ( ShiroDkxtro2#8750 )
+// To be used with $additive. Do not shine a flashlight on a model using this. In my experience that will just crash the game for some reason.
+//	I did not have the time to debug that and its unnecessary. Who has a flashlight in their 3d skybox anyways?
 //
 //==================================================================================================
 
@@ -11,10 +13,6 @@
 // Includes for PS30
 #include "atmosphere_vs30.inc"
 #include "atmosphere_ps30.inc"
-
-// Includes for PS20b
-//#include "include/Atmosphere_vs20.inc"
-//#include "include/Atmosphere_ps20b.inc"
 
 // This is not technically required, but makes it easier to understand later
 const Sampler_t SAMPLER_BASETEXTURE = SHADER_SAMPLER0;
@@ -101,10 +99,10 @@ SHADER_INIT
 	SET_FLAGS(MATERIAL_VAR_MODEL);
 
 		SET_FLAGS2(MATERIAL_VAR2_SUPPORTS_HW_SKINNING);             // Required for skinning
-		SET_FLAGS2(MATERIAL_VAR2_DIFFUSE_BUMPMAPPED_MODEL);         // Required for dynamic lighting, even though we have no bumping we set it BECAUSE : no particular reason
+		SET_FLAGS2(MATERIAL_VAR2_DIFFUSE_BUMPMAPPED_MODEL);         // Required for dynamic lighting
 		SET_FLAGS2(MATERIAL_VAR2_NEEDS_TANGENT_SPACES);             // Required for dynamic lighting
 		SET_FLAGS2(MATERIAL_VAR2_LIGHTING_VERTEX_LIT);              // Required for dynamic lighting
-		SET_FLAGS2(MATERIAL_VAR2_NEEDS_BAKED_LIGHTING_SNAPSHOTS);   // Required for ambient cube, even though we have no bumping we set it BECAUSE : no particular reason
+		SET_FLAGS2(MATERIAL_VAR2_NEEDS_BAKED_LIGHTING_SNAPSHOTS);   // Required for ambient cube
 		SET_FLAGS2(MATERIAL_VAR2_SUPPORTS_FLASHLIGHT);              // Required for flashlight
 		SET_FLAGS2(MATERIAL_VAR2_USE_FLASHLIGHT);                   // Required for flashlight
 };
@@ -131,7 +129,7 @@ SHADER_DRAW
 		pShaderShadow->AlphaFunc(SHADER_ALPHAFUNC_GEQUAL, params[info.alphaTestReference]->GetFloatValue());
 	}
 
-	// Basically : not in hammer
+	// Basically : Not in Hammer(?)
 	if (IsSnapshotting())
 	{
 		// If alphatest is on, enable it. Well it always is... so yeah haha
@@ -150,8 +148,8 @@ SHADER_DRAW
 		int nShadowFilterMode = bHasFlashlight ? g_pHardwareConfig->GetShadowFilterMode() : 0;
 
 		// Setting up samplers
-		pShaderShadow->EnableTexture(SAMPLER_BASETEXTURE, true);    // Basecolor texture
-		pShaderShadow->EnableSRGBRead(SAMPLER_BASETEXTURE, true);   // Basecolor is sRGB
+		pShaderShadow->EnableTexture(SAMPLER_BASETEXTURE, true);    // Basetexture
+		pShaderShadow->EnableSRGBRead(SAMPLER_BASETEXTURE, true);   // Basetexture is sRGB
 
 		// If the flashlight is on, set up its textures
 		if (bHasFlashlight)
@@ -187,10 +185,10 @@ SHADER_DRAW
 		SET_STATIC_PIXEL_SHADER(atmosphere_ps30);
 
 		// Setting up fog
-		DefaultFog(); // I think this is correct
+		DefaultFog();
 
 		// HACK HACK HACK - enable alpha writes all the time so that we have them for underwater stuff
-		// WRD: If you put this thing underwater I will annihilate your entire family line
+		// WRD: Do not do this if you want it to render in the 3d skybox, I experienced issues with it.
 		// pShaderShadow->EnableAlphaWrites(bFullyOpaque);
 	}
 	else // Not snapshotting -- begin dynamic state
@@ -212,7 +210,7 @@ SHADER_DRAW
 		// WRD : What this does is get the lightstate. Yeah right I know thats what It just said. More specifically it gets whether to use following lightstates
 		// lightState.m_nNumLights, lightState.m_bAmbientLight, lightState.m_bStaticLightTexel, lightState.m_bStaticLightVertex
 		// There is also a dynamic one, anyways although technically overridable, not advised since it can drastically fuck up the lighting
-		// Also StaticLightTexel appears to be entirely unused *and* doesn't exist in sdk2013sp.
+		// Also StaticLightTexel, StaticLightVertex, NumLights appear to not exist in sdk2013sp.
 		LightState_t lightState;
 		pShaderAPI->GetDX9LightState(&lightState);
 
@@ -261,24 +259,24 @@ SHADER_DRAW
 		// Setting up dynamic vertex shader
 		DECLARE_DYNAMIC_VERTEX_SHADER(atmosphere_vs30);
 		SET_DYNAMIC_VERTEX_SHADER_COMBO(DYNAMIC_LIGHT, lightState.HasDynamicLight());
-		SET_DYNAMIC_VERTEX_SHADER_COMBO(STATIC_LIGHT_VERTEX, 1);
+		SET_DYNAMIC_VERTEX_SHADER_COMBO(STATIC_LIGHT_VERTEX, 1); HackHack - This Lightstate doesn't exist in SP so I just forced it to the default value
 		SET_DYNAMIC_VERTEX_SHADER_COMBO(DOWATERFOG, fogIndex);
 		SET_DYNAMIC_VERTEX_SHADER_COMBO(SKINNING, numBones > 0);
 		SET_DYNAMIC_VERTEX_SHADER_COMBO(LIGHTING_PREVIEW, pShaderAPI->GetIntRenderingParameter(INT_RENDERPARM_ENABLE_FIXED_LIGHTING) != 0);
 		SET_DYNAMIC_VERTEX_SHADER_COMBO(COMPRESSED_VERTS, (int)vertexCompression);
-		SET_DYNAMIC_VERTEX_SHADER_COMBO(NUM_LIGHTS, 4);
+		SET_DYNAMIC_VERTEX_SHADER_COMBO(NUM_LIGHTS, 4); // HackHack - This Lightstate doesn't exist in SP so I just forced it to the default value
 		SET_DYNAMIC_VERTEX_SHADER(atmosphere_vs30);
 
 		// Setting up dynamic pixel shader
 		DECLARE_DYNAMIC_PIXEL_SHADER(atmosphere_ps30);
-		//SET_DYNAMIC_PIXEL_SHADER_COMBO(NUM_LIGHTS, lightState.m_nNumLights);
+		//SET_DYNAMIC_PIXEL_SHADER_COMBO(NUM_LIGHTS, lightState.m_nNumLights); HackHack - This Lightstate doesn't exist in SP so I just forced it to the default value
 		SET_DYNAMIC_PIXEL_SHADER_COMBO(WRITEWATERFOGTODESTALPHA, bWriteWaterFogToAlpha);
 		SET_DYNAMIC_PIXEL_SHADER_COMBO(WRITE_DEPTH_TO_DESTALPHA, bWriteDepthToAlpha);
 		SET_DYNAMIC_PIXEL_SHADER_COMBO(PIXELFOGTYPE, pShaderAPI->GetPixelFogCombo());
 		//SET_DYNAMIC_PIXEL_SHADER_COMBO(FLASHLIGHTSHADOWS, bFlashlightShadows);
 		SET_DYNAMIC_PIXEL_SHADER(atmosphere_ps30);
 
-		// This is probably important
+		// What ColorSpace to use
 		SetModulationPixelShaderDynamicState_LinearColorSpace(PSREG_DIFFUSE_MODULATION);
 
 		// Send lighting array to the pixel shader
@@ -326,8 +324,6 @@ SHADER_DRAW
 			pShaderAPI->SetPixelShaderConstant(PSREG_ENVMAP_TINT__SHADOW_TWEAKS, tweaks, 1);
 		}
 
-	// just here as a reference for now
-	// pShaderAPI->SetPixelShaderConstant(27, flParams, 1);
 		float AtmosphereControl[4] = {1, 0, 0, 1};
 
 		if (bHasTransparencyFactor)
@@ -339,10 +335,9 @@ SHADER_DRAW
 		{
 			AtmosphereControl[3] = params[info.GradientFactor]->GetFloatValue();
 		}
-		// last 1 here is whether or not to force
-		pShaderAPI->SetPixelShaderConstant(27, AtmosphereControl, 1);
+		
 
-		// Original shaders didn't do this, and it disgusts me because the g_EyePos on the shader doesn't consider 3d skybox lol
+		pShaderAPI->SetPixelShaderConstant(27, AtmosphereControl, 1);
 		pShaderAPI->SetPixelShaderConstant(5, vEyePos_SpecExponent, 1);
 
 	} // WRD : IMPORTANT!!!!! This must be above Draw. I f'd it up once, never again, therefore this note is here lol
